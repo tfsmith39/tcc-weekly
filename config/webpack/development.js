9b430cfcd5ad123fd0d5711c246907ed085c4694
Tim Smith <tsmith39@tulane.edu>
2020-01-08 00:50:51 -0500

    webpack issues..

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader',
+                    'style-loader'
+                ]
+            }
+        ]
+    }
+};
