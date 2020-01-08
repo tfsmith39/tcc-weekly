6ff554cd19108a93dc54783c7051431da9b65a30
Tim Smith <tsmith39@tulane.edu>
2020-01-08 00:35:48 -0500

    config webpack edits

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+          }
+        ]
+    }
+};
