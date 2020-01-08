dfe501826e7463104440f75d91b4446d119609f8
Tim Smith <tsmith39@tulane.edu>
2020-01-08 01:00:05 -0500

    webpack issues..

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,7 +5,7 @@ const environment = require('./environment')
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: '../../app/javascript/channels/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
